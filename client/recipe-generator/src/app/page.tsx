6cfcc7bd89bdb328bd8e0cd6cde8e97ef18a77ba
Josh Nelson <joshtn333@gmail.com>
2024-12-09 23:16:48 -0600

    visual touchups

--- client/recipe-generator/src/app/page.tsx
+++ client/recipe-generator/src/app/page.tsx
@@ -1,5 +1,3 @@
-// import Image from "next/image";
-
 "use client";
 
 import React, { useState } from "react";
@@ -7,7 +5,7 @@ import getResponse from "./api/route";
 
 export default function ChatPage() {
   const [messages, setMessages] = useState([
-    { text: "Hello user! List ingredients to get recipe suggestions! Remember to put a comma (,) in between each ingredient listed", sender: "bot" }
+    { text: "Hi! Start searching for recipes below!", sender: "bot" }
   ]);
   const [input, setInput] = useState("");
   const [loading, setLoading] = useState(false);
@@ -24,6 +22,7 @@ export default function ChatPage() {
       setInput("");
       const response = await getResponse(query);
       console.log('Response:', response);
+      setMessages((prevMessages) => [...prevMessages.slice(0, -1)]);
       const lines = response.split("\n\n\n")
       lines.map((line: string, index: number) => {
         setMessages((prevMessages) => [...prevMessages, { text: line, sender: "bot" }]);
@@ -55,44 +54,44 @@ export default function ChatPage() {
 
 
   return (
-    <div className="bg-gray-200 min-h-screen flex items-center justify-center p-5">
+    <div className="bg-gray-200 min-h-screen flex items-end justify-center px-[12vw] pb-10 pt">
       {/* Label at the Top */}
-      <div className="absolute top-0 w-full text-center py-4 font-bold text-2xl bg-gray-500 text-gray-100 shadow-sm">
+      <div className="absolute top-5 w-full text-center py-4 font-bold text-6xl text-gray-700 font-serif">
         Recipe Recommender System
       </div>
 
       {/* Chatbox Container */}
-      <div className="flex flex-col max-w-3xl w-full mx-auto p-10 rounded-lg shadow-lg bg-gray-400 mt-20 border-4 border-gray-400">
-        {/* Messages container */}
-        <div className="flex flex-col space-y-3 overflow-y-auto h-[35rem] p-5">
-          {messages.map((msg, index) => (
-            <div
-              key={index}
-              className={`${
-                msg.sender === "user" ? "bg-blue-500 text-white self-end" : "bg-gray-300 text-gray-900 self-start"
-              } p-3 rounded-lg max-w-xs`}
-            >
-              {formatText2(msg.text)}
-            </div>
-          ))}
-        </div>
-        
+      <div className="h-[80vh] flex flex-col-reverse justify-start w-full mx-auto p-10 rounded-lg shadow-lg bg-gray-400 border-4 border-gray-400 text-lg">        
         {/* Input and Send button */}
-        <div className="flex items-center border-t border-gray-300 pt-3 mt-3">
+        <div className="flex items-center border-t border-gray-200 pt-3 mt-3">
           <input
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && handleSend()}
-            placeholder="Type ingredients here..."
-            className="flex-grow p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+            placeholder="Type here..."
+            className="flex-grow p-2 rounded-lg border bg-gray border-gray focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
           />
           <button
             onClick={handleSend}
-            className="ml-3 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+            className="inline-flex items-center justify-center rounded-md text-sm font-medium h-10 px-4 py-2 ml-3 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
           >
-            {'->'}
+            Send
           </button>
+        </div>
+
+        {/* Messages container */}
+        <div className="flex flex-col space-y-3 overflow-y-auto p-5">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`${
+                msg.sender === "user" ? "bg-blue-500 text-white self-end" : "bg-gray-200 text-gray-900 self-start"
+              } p-3 rounded-lg max-w-[50vw]`}
+            >
+              {formatText2(msg.text)}
+            </div>
+          ))}
         </div>
       </div>
     </div>
